24bb567936b26a2f1aa846316cebb2ee863e03f7
Dimitar Nikolov <49731322+dimitarnikolovv@users.noreply.github.com>
2022-12-01 16:08:35 +0200

    calendar events fixes

--- src/components/EventList.svelte
+++ src/components/EventList.svelte
@@ -1,20 +1,51 @@
 <script>
     import { marked } from 'marked';
+    import { fade } from 'svelte/transition';
 
     export let events;
     export let date = { day: 0, month: '' };
     export let onClose;
+
+    function clickOutside(node) {
+        const handleClick = (event) => {
+            if (node && !node.contains(event.target) && !event.defaultPrevented) {
+                node.dispatchEvent(new CustomEvent('click_outside', node));
+            }
+        };
+
+        document.addEventListener('click', handleClick, true);
+
+        return {
+            destroy() {
+                document.removeEventListener('click', handleClick, true);
+            },
+        };
+    }
 </script>
 
-<div class="event-list">
+<div
+    class="event-list"
+    use:clickOutside
+    on:click_outside={onClose}
+    transition:fade={{ duration: 100 }}
+>
     <header>
         <h5>Събития за {date.day}. {date.month}</h5>
-        <button class="close" on:click={onClose}>x</button>
+        <span class="material-symbols-rounded" on:click={onClose} on:keypress={onClose}>close</span>
     </header>
 
     {#each events as event}
         <div class="event-entry">
             <h5>{event.title}</h5>
+            <span
+                >{new Date(event.startsAt).toLocaleTimeString('bg-BG', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                })} - {new Date(event.endsAt).toLocaleTimeString('bg-BG', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                })}</span
+            >
             <p>{event.description}</p>
             <div class="content-wrapper">{@html marked(event.content)}</div>
         </div>
@@ -38,6 +69,10 @@
         box-shadow: 0 15px 40px rgba(0, 0, 0, 0.12);
         border-radius: 10px;
 
+        &::-webkit-scrollbar {
+            width: 1px;
+        }
+
         h5 {
             font-size: 1.4rem;
         }
@@ -51,23 +86,26 @@
         header {
             display: flex;
             justify-content: space-between;
+            gap: 1.5rem;
             padding-block-end: 0.5rem;
             width: 100%;
             border-bottom: 1px solid rgba(0, 0, 0, 0.12);
-        }
 
-        .close {
-            font-size: 1.2rem;
+            span {
+                font-size: 1.2rem;
 
-            cursor: pointer;
-            border-radius: 50%;
-            width: 1.3em;
-            height: 1.3em;
-            border: none;
-            background: none;
+                cursor: pointer;
+                border-radius: 50%;
+                width: 1.3em;
+                height: 1.3em;
+                border: none;
+                background: none;
+                text-align: center;
+                line-height: 1.3em;
 
-            &:hover {
-                background-color: #f2f2f2;
+                &:hover {
+                    background-color: #f2f2f2;
+                }
             }
         }
     }
